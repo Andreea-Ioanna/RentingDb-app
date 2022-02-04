92d6c0be148adb8ab48aaa09ba20e34c179537db
AdiGeorge <43153481+AdiGeorge@users.noreply.github.com>
2022-02-04 06:04:54 +0100

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,17 +2,21 @@
     <div class="landlord_profile_container">
         <div class="landlord_header">
             <div>
+                <div class="user_header">
+                <img src="/assets/user.png">
                 <h1 class="welcome">Welcome {{ Auth::user()->name }}</h1>
-                <div class="underline"></div>
+                </div>
             </div>
         </div>
 
         <div class="lanlord_main">
             <div class="landlord_properties">
-                <h3>Your properties</h3>
-                <div class="subpage">
-             
-                </div>
+                <h3>Here you can see your appointments</h3>
+                <div class="underline"></div>
+               <div class="appointments_container">
+                   <img src="/assets/calendar.png">
+                   <h3>No appointments yet</h3>
+               </div>
             </div>
         </div>
     </div>
